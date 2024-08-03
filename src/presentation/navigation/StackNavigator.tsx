69d11a37c204ae7040f0e3dd7d36df55c6136f85
emanuelhardwell <emanuelnoe16@gmail.com>
2024-08-03 11:29:41 -0600

    feat: Update PermissionsScreen with usePermissionStore

--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -13,7 +13,8 @@ const Stack = createStackNavigator<RootStackParams>();
 export const StackNavigator = () => {
   return (
     <Stack.Navigator
-      initialRouteName="LoadingScreen"
+      // initialRouteName="LoadingScreen"
+      initialRouteName="PermissionsScreen"
       screenOptions={{
         headerShown: false,
         cardStyle: {backgroundColor: 'white'},
--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,9 +1,27 @@
-import {Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {globalStyles} from '../../../config/theme/global-styles';
+import {usePermissionStore} from '../../store/permission/usePermissionStore';
 
 export const PermissionsScreen = () => {
+  const {permissionStatus, requestLocationPermission} = usePermissionStore();
+
   return (
-    <View>
-      <Text>PermissionsScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Habilitar Ubicación</Text>
+
+      <Pressable
+        style={globalStyles.btnPrimary}
+        onPress={requestLocationPermission}>
+        <Text style={styles.text}>Habilitar localización</Text>
+      </Pressable>
+
+      <Text>Estado actual: {permissionStatus}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  text: {color: 'white'},
+  textTitle: {fontSize: 16, fontWeight: 'bold'},
+});
